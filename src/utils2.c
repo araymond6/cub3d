3be6f26f02b3f67a0962bfbcae74dab31a63a1bb
Aurelia Raymond <araymond@c1r5p8.42quebec.com>
2023-12-11 10:45:59 -0500

    working on starting pos and direction

--- src/utils2.c
+++ src/utils2.c
@@ -1,5 +1,35 @@
 #include "../include/cub3D.h"
 
+void	set_direction(t_cub *cub)
+{
+	if (cub->map.playerdir == 'N')
+	{
+		cub->v.playerdir[1] = 0;
+		cub->v.playerdir[0] = -1;
+		cub->v.camplane[0] = -0.66;
+	}
+	else if (cub->map.playerdir == 'S')
+	{
+		cub->v.playerdir[1] = 0;
+		cub->v.playerdir[0] = 1;
+		cub->v.camplane[0] = 0.66;
+	}
+	else if (cub->map.playerdir == 'E')
+	{
+		cub->v.playerdir[1] = 0;
+		cub->v.playerdir[0] = 1;
+		cub->v.camplane[0] = 0;
+		cub->v.camplane[1] = 0.66;
+	}
+	else if (cub->map.playerdir == 'W')
+	{
+		cub->v.playerdir[1] = 0;
+		cub->v.playerdir[0] = -1;
+		cub->v.camplane[0] = 0;
+		cub->v.camplane[1] = -0.66;
+	}
+}
+
 void	init_cub(t_cub *cub) // TODO: move to utils
 {
 	cub->v.pos[0] = (double)5 / 8 + 0.5;
@@ -8,6 +38,7 @@ void	init_cub(t_cub *cub) // TODO: move to utils
 	cub->v.step[1] = 1;
 	cub->v.camangle = 0.5;
 
+	set_direction(cub);
 	// for the direction, to change when 
 	cub->v.playerdir[1] = 0; // 0 for north & south
 	cub->v.playerdir[0] = 1;
@@ -17,8 +48,6 @@ void	init_cub(t_cub *cub) // TODO: move to utils
 	cub->v.rotspeed = 0.00006;
 	cub->ceiling = get_color(cub->map.c_red, cub->map.c_green, cub->map.c_blue, 255);
 	cub->floor = get_color(cub->map.f_red, cub->map.f_green, cub->map.f_blue, 255);
-	printf("c: %d, %d, %d\n", cub->map.c_red, cub->map.c_green, cub->map.c_blue);
-	printf("f: %d, %d, %d\n", cub->map.f_red, cub->map.f_green, cub->map.f_blue);
 }
 
 void	close_hook(void *param)
